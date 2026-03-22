1034700298247869ad6de1a578ed4bb56fe6cc6e
cirdw <promitder123@gmail.com>
2026-03-22 11:59:06 +0000

    implemented few some funtionality

--- src/script.js
+++ src/script.js
@@ -1,32 +1,49 @@
- document.addEventListener("DOMContentLoaded", function() {
-            console.log("Hello Adverty");
-            const display = document.getElementById("calc-display");
-            const buttons = document.getElementsByClassName("btn");
-            
-            let currentVal = "";
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Hello Adverty");
+    const display = document.getElementById("calc-display");
+    const buttons = document.getElementsByClassName("btn");
 
-            function evaluateResult(){
-                const convertedVal = currentVal.replace("X", "*").replace("%", "*0.01");
-                const result = eval(convertedVal);
-                display.value = result;
+    let currentVal = "";
+
+    function evaluateResult() {
+        const convertedVal = currentVal.replace("X", "*")
+            .replace("%", "*0.01")
+            .replace("sin", "Math.sin")
+            .replace("cos", "Math.cos")
+            .replace("π", "Math.PI")
+            .replace("ln", "Math.log")
+            .replace("tan", "Math.tan")
+            .replace("log", "Math.log10")
+            .replace("√", "Math.sqrt")
+            .replace("e", "Math.E");
+        const result = eval(convertedVal);
+        display.value = result;
+    }
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
+
+
+            try {
+                if (value == "AC") {
+                    currentVal = "";
+                    display.value = currentVal;
+                }
+                else if (value == "=") {
+                    evaluateResult();
+                }
+                else {
+                    currentVal += value;
+                    display.value = currentVal;
+                }
             }
-            for(let i=0; i< buttons.length; i++){
-                const button = buttons[i];
-                button.addEventListener('click', function(){
-                    const value = button.innerText;
-                    
-                    if(value == "AC"){
-                        currentVal = "";
-                        display.value = currentVal;
-                    }
-                    else if(value == "="){
-                        evaluateResult();
-                    }
-                    else {
-                        currentVal += value;
-                        display.value = currentVal;
-                    }
+            catch (error){
+                console.error(error);
+                currentVal = "Error";
+                display.value = currentVal;
+            }        
                     
                 });
-            }
-        });
+    }
+});
